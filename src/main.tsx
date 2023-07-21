570eef3d7520f98e0a748af2e7ff86161a50a5e0
Derek Milne <derek.m.m.77@gmail.com>
2023-07-21 10:23:34 -0700

    messaging page stub

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "the-new-css-reset/css/reset.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MessagingPage from "./routes/messaging/messaging.page.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MessagingPage />,
   },
 ]);
 
---
+++ src/routes/messaging/messaging.page.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function MessagingPage(): ReactElement {
+  return (
+    <div>
+      <h1>Messaging Page</h1>
+    </div>
+  );
+}
