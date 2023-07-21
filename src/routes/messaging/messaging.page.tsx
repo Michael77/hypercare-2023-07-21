a8e21c8aa98e984c37a2d9219410d3b2cf73c1aa
Derek Milne <derek.m.m.77@gmail.com>
2023-07-21 10:46:13 -0700

    lato global font

--- src/routes/messaging/messaging.page.tsx
+++ src/routes/messaging/messaging.page.tsx
@@ -3,7 +3,7 @@ import classes from "./messaging.module.scss";
 
 export default function MessagingPage(): ReactElement {
   return (
-    <div>
+    <div className={classes.messagingPage}>
       <h1 className={classes.customH1}>Messaging Page</h1>
     </div>
   );
