4dc70b0ca229779b8fa7bff74c6881696842a4db
Derek Milne <derek.m.m.77@gmail.com>
2023-07-21 12:21:45 -0700

    chat tile layout

--- src/routes/messaging/components/chat-tile/chat-tile.component.tsx
+++ src/routes/messaging/components/chat-tile/chat-tile.component.tsx
@@ -2,9 +2,29 @@ import { ReactElement } from "react";
 import classes from "./chat-tile.module.scss";
 import { BasicChatFields } from "../../../../models/models.ts";
 import { RecursivePartial } from "../../../../type-helpers.ts";
+import UserAvatar from "../../../../components/user-avatar/user-avatar.component.tsx";
 
 export default function ChatTile(props: {
   chat: RecursivePartial<BasicChatFields>;
 }): ReactElement {
-  return <div className={classes.chatTile}>{JSON.stringify(props.chat)}</div>;
+  const { chat } = props;
+
+  function getChatTitle(): string {
+    return chat.title ?? "";
+  }
+
+  return (
+    <div className={classes.chatTile}>
+      <UserAvatar></UserAvatar>
+
+      <div>
+        <h2>{getChatTitle()}</h2>
+        <p></p>
+      </div>
+
+      <div>
+        <time>{chat.dateCreated}</time>
+      </div>
+    </div>
+  );
 }
