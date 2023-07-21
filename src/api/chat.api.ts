4f5f4f8ffd1b4ac2209a76b200eebba0f8706f89
Derek Milne <derek.m.m.77@gmail.com>
2023-07-21 13:05:26 -0700

    correctly scroll chat list

--- src/api/chat.api.ts
+++ src/api/chat.api.ts
@@ -38,4 +38,104 @@ const mockChatFields: ChatFields = [
     },
     members: [mockUsers.derek, mockUsers.emily],
   },
+  {
+    type: "self",
+    title: "My very own chat",
+    lastMessage: {
+      dateCreated: "2023-07-21T19:08:03+00:00",
+      message: "Hey dude",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek],
+  },
+  {
+    type: "group",
+    title: "Group chat",
+    lastMessage: {
+      dateCreated: "2023-06-21T19:08:03+00:00",
+      message: "It's me again",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek, mockUsers.emily],
+  },
+  {
+    type: "self",
+    title: "My very own chat",
+    lastMessage: {
+      dateCreated: "2023-07-21T19:08:03+00:00",
+      message: "Hey dude",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek],
+  },
+  {
+    type: "group",
+    title: "Group chat",
+    lastMessage: {
+      dateCreated: "2023-06-21T19:08:03+00:00",
+      message: "It's me again",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek, mockUsers.emily],
+  },
+  {
+    type: "self",
+    title: "My very own chat",
+    lastMessage: {
+      dateCreated: "2023-07-21T19:08:03+00:00",
+      message: "Hey dude",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek],
+  },
+  {
+    type: "group",
+    title: "Group chat",
+    lastMessage: {
+      dateCreated: "2023-06-21T19:08:03+00:00",
+      message: "It's me again",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek, mockUsers.emily],
+  },
+  {
+    type: "self",
+    title: "My very own chat",
+    lastMessage: {
+      dateCreated: "2023-07-21T19:08:03+00:00",
+      message: "Hey dude",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek],
+  },
+  {
+    type: "group",
+    title: "Group chat",
+    lastMessage: {
+      dateCreated: "2023-06-21T19:08:03+00:00",
+      message: "It's me again",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek, mockUsers.emily],
+  },
+  {
+    type: "self",
+    title: "My very own chat",
+    lastMessage: {
+      dateCreated: "2023-07-21T19:08:03+00:00",
+      message: "Hey dude",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek],
+  },
+  {
+    type: "group",
+    title: "Group chat",
+    lastMessage: {
+      dateCreated: "2023-06-21T19:08:03+00:00",
+      message: "It's me again",
+      sender: mockUsers.derek,
+    },
+    members: [mockUsers.derek, mockUsers.emily],
+  },
 ];
