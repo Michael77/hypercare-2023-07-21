22c9af2a9d0fc6f8f269219a0f54c37b81ec2fd4
Derek Milne <derek.m.m.77@gmail.com>
2023-07-21 11:02:14 -0700

    chat list layout

--- src/routes/messaging/messaging.page.tsx
+++ src/routes/messaging/messaging.page.tsx
@@ -6,12 +6,18 @@ export default function MessagingPage(): ReactElement {
     <div className={classes.messagingPage}>
       <header className={classes.pageHeader}>
         <div className={classes.headingCont}>
-          <h3>Messaging</h3>
+          <h1>Messaging</h1>
         </div>
         <div className={classes.headingCont}>
-          <h3>IPC Team</h3>
+          <h1>IPC Team</h1>
         </div>
       </header>
+
+      <main className={classes.pageMain}>
+        <div className={classes.chatList}></div>
+
+        <div className={classes.chatDisplay}></div>
+      </main>
     </div>
   );
 }
