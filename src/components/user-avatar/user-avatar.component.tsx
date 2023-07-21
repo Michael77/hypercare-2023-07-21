519f17083bd87d4913a9d711da994193dd598f4f
Derek Milne <derek.m.m.77@gmail.com>
2023-07-21 11:05:45 -0700

    component stubs

---
+++ src/components/user-avatar/user-avatar.component.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import classes from "./user-avatar.module.scss";
+
+export default function UserAvatar(): ReactElement {
+  return <div className={classes.userAvatar}></div>;
+}
---
+++ src/routes/messaging/components/chat-tile/chat-tile.component.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import classes from "./chat-tile.module.scss";
+
+export default function ChatTile(): ReactElement {
+  return <div className={classes.chatTile}></div>;
+}
