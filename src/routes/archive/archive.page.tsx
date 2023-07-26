d80a2c4dcd4cb02ebaef283e1d2e71aefc070e94
Derek Milne <derek.m.m.77@gmail.com>
2023-07-26 11:41:48 -0700

    archive page layout

--- src/routes/archive/archive.page.tsx
+++ src/routes/archive/archive.page.tsx
@@ -1,6 +1,9 @@
 import { ReactElement, useContext, useEffect } from "react";
 import classes from "./archive.module.scss";
 import { ChatStoreContext } from "../../stores/chat.store.tsx";
+import LoadingSpinner from "../../components/loading-spinner/loading-spinner.component.tsx";
+import ChatTile from "../messaging/components/chat-tile/chat-tile.component.tsx";
+import { ChatFields } from "../../models/models.ts";
 
 export default function ArchivePage(): ReactElement {
   const chatStoreContext = useContext(ChatStoreContext);
@@ -9,5 +12,37 @@ export default function ArchivePage(): ReactElement {
     chatStoreContext?.loadChats();
   }, []);
 
-  return <div className={classes.messagingPage}></div>;
+  function getSortedArchivedChats(chats: ChatFields | undefined): ChatFields {
+    return [];
+  }
+
+  return (
+    <div className={classes.archivePage}>
+      <header className={classes.pageHeader}>
+        <div className={classes.headingCont}>
+          <h1>Archived</h1>
+          <a href="/">X</a>
+        </div>
+        <div className={classes.headingCont}>
+          <h1>IPC Team</h1>
+        </div>
+      </header>
+
+      <main className={classes.pageMain}>
+        <div className={classes.chatList}>
+          {chatStoreContext?.loadingChatFields ? (
+            <div className={classes.loadingSpinner}>
+              <LoadingSpinner></LoadingSpinner>
+            </div>
+          ) : (
+            getSortedArchivedChats(chatStoreContext?.chatFields).map((chat) => (
+              <ChatTile chat={chat}></ChatTile>
+            ))
+          )}
+        </div>
+
+        <div className={classes.chatDisplay}></div>
+      </main>
+    </div>
+  );
 }
